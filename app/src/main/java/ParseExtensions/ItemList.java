139604702aeccba2fac05b24b3ea6b3cd9fccc67
Trond <trond_faa_89@hotmail.com>
2018-08-17 15:34:50 +0200

    Added ParseExtensions

--- app/src/main/java/ParseExtensions/ItemList.java
+++ app/src/main/java/ParseExtensions/ItemList.java
@@ -1,7 +1,22 @@
 package ParseExtensions;
 
 
+import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
+@ParseClassName("ParseDetailsItem")
 public class ItemList extends ParseObject {
+
+    public ItemList(String description){
+        put("Description",description);
+    }
+
+    public String getDescription(){
+        return getString("Description");
+    }
+
+    public void setDescription(String newDescription){
+        put("Description",newDescription);
+        saveInBackground();
+    }
 }
